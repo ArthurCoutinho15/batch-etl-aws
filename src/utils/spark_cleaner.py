292597b7573e54ec50ffebc74cd6f706f15a5563
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-08-30 16:32:30 -0300

    feat: adiciona spark cleaner

---
+++ src/utils/spark_cleaner.py
@@ -0,0 +1,26 @@
+from typing import List
+
+import pyspark.sql.functions as F 
+from pyspark.sql import DataFrame
+
+
+
+class SparkCleanData():
+    
+    @staticmethod
+    def explode_json(spark_dataframe: DataFrame, columns_explode: List[str]) -> DataFrame:
+        for column in columns_explode:
+            spark_dataframe = spark_dataframe.withColumn(column, F.explode(F.col(column)))
+            
+        return spark_dataframe
+    
+    @staticmethod
+    def select_exploded_columns(spark_dataframe: DataFrame, select_columns: list = None) -> DataFrame:
+        
+        if select_columns:
+            spark_dataframe = spark_dataframe.select(*select_columns)
+        return spark_dataframe
+    
+    @staticmethod
+    def save_bronze(spark_dataframe: DataFrame, ingestion_path = str):
+        spark_dataframe.write.mode("overwrite").csv(ingestion_path)
